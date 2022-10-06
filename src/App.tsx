03cb69e612703e302acb7e7a708454ceaedb2571
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-06 21:12:40 +0900

    7. interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    function sum(a: number, b: number): number {
-        return a + b;
+    // interface
+
+    interface Contact {
+        name: string;
+        phone: number;
+        email: string;
     }
 
-    const bigNum: bigint = BigInt(10);
-    console.log(typeof bigNum)
-
-    function waitAndDo(callback: (param: string) => void) {
-        setTimeout(() => {
-            callback("Oops!");
-        }, 1000)
-    }
+    const contacts: Contact[] = [];
 
     return (
         <div></div>
