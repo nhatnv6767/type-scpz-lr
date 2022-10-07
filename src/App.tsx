dd2be842741638bfda0ffbe21da160d75322257d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-07 21:57:10 +0900

    8. Optional properties and strictNullCheck 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,6 @@ import './App.css';
 function App() {
     // interface
 
-    interface Contact {
-        name: string;
-        phone: string;
-        email: string;
-    }
-
-    const contacts: Contact[] = [];
-
-    const newContact: Contact = {
-        name: "Nguyen A A",
-        phone: "012333212",
-        email: "nothing@gmail.com",
-    }
-
-    const otherContact: Contact = {
-        name: "Nguyen A B",
-        phone: "0123f33212",
-    }
-
-    contacts.push(newContact);
 
     return (
         <div></div>
---
+++ src/lectures/index.ts
@@ -0,0 +1,20 @@
+interface Contact {
+    name: string;
+    phone: string;
+    email: string;
+}
+
+const contacts: Contact[] = [];
+
+const newContact: Contact = {
+    name: "Nguyen A A",
+    phone: "012333212",
+    email: "nothing@gmail.com",
+}
+
+const otherContact: Contact = {
+    name: "Nguyen A B",
+    phone: "0123f33212",
+}
+
+contacts.push(newContact);
