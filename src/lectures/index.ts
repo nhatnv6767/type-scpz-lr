0416547a0ddd32c8765b2c14ec66697f276ef1ff
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-13 21:09:22 +0900

    learning 13

--- src/lectures/index.ts
+++ src/lectures/index.ts
@@ -1,34 +1,6 @@
-// String
-
+// Object type
 // @ts-ignore
-let firstName = "String";
-let lastName: string;
 
-lastName = "Bh none"
+let singer = {};
 
-// number
-// let age = 24;
-
-let age: number;
-age = 24;
-age = "string".length;
-
-
-// boolean
-
-let isValid = true;
-
-isValid = false;
-
-// undefined, null
-let k: undefined;
-let m: null;
-
-
-// any type
-let anyType: any;
-
-anyType = 24;
-anyType = "string";
-anyType = true;
-
+singer.name = "hello";
